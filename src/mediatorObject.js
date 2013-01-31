2501036b01b5cbce94ae86e023cca33168a70600
unknown <fighter2929@hotmail.com>
2013-01-31 01:00:18 -0200

    Starting tests with the mediator pattern

--- src/mediatorObject.js
+++ src/mediatorObject.js
@@ -3,6 +3,7 @@ Source:
 - http://arguments.callee.info/2009/05/18/javascript-design-patterns--mediator/
 - http://addyosmani.com/largescalejavascript/
 - https://developers.google.com/closure/compiler/docs/js-for-compiler
+- http://www.oodesign.com/mediator-pattern.html
 ***/
 
 //TODO: Anonymous function to control global variables
@@ -24,7 +25,7 @@ function Mediator( idString )
 }
 
 Mediator.prototype.constructor = Mediator;
-Mediator.prototype.toString = function() { return this.id; }
+Mediator.prototype.toString = function() { return "Mediator with ID: " + this.id; }
 
 Mediator.prototype.debugAlert = function(text) {
 	alert( text );
@@ -35,11 +36,13 @@ Mediator.prototype.debugAlert = function(text) {
  *
  * @param {string} event - The event that has been published
  * @param {...[?]} args -  A variable number of variables of different types that the event can use
- * @param {string} source - The channel to which the event is going to be send
+ * @param {string} source - The object where the callback function will be called, 
+ * if it is undefined, then it will be called in the component itself (the object which contains the callback function)
  */
 Mediator.prototype.publish = function(event, args, source) {
+	var reachCount = 0;
 	if (!event) {
-		return;
+		return reachCount;
 	}
 	// Checking publish arguments
 	args = args || [];
@@ -50,11 +53,13 @@ Mediator.prototype.publish = function(event, args, source) {
 				//this.debugAlert("Mediator calling " + event + " on " + c); // using this because of the try-catch scope
 				source = source || this.components[c];
 				this.components[c]["on" + event].apply(source, args);
+				reachCount++;
 			} catch (err) {
 				this.debugAlert(["Mediator error.", event, args, source, err].join(' '));
 			}
 		}
 	}
+	return reachCount;
 }
 
 
@@ -62,8 +67,8 @@ Mediator.prototype.publish = function(event, args, source) {
  * Subscribe a component to receive information about updates in a channel 
  *
  * @param {string} name - An unique name to identify the component that wants to be notified by this mediator
- * @param {?} component -  The component object with the callback functions to subscribe
  * @param {boolean} replaceDuplicate - If a new component can replace an old with the same name in the case that it`s already subscribed
+ * @param {?} component -  The component object with the callback functions to subscribe
  */
 Mediator.prototype.subscribe = function(name, replaceDuplicate, component ) {
 	if (name in this.components) {
@@ -80,4 +85,9 @@ Mediator.prototype.removeComponent = function(name) {
 	if (name in this.components) {
 		delete this.components[name];
 	}
+}
+
+Mediator.prototype.getComponent = function(name) 
+{
+	return this.components[name]; // undefined if component has not been added
 }
