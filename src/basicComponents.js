9afeb7e2db63d9effdf36c63be407c00cc0f4bfa
unknown <fighter2929@hotmail.com>
2013-03-10 00:46:11 -0300

    Starting to create the pattern to change objects' shape and its components

--- src/basicComponents.js
+++ src/basicComponents.js
@@ -1,5 +1,6 @@
-function Component( baseObject )
+function Component( baseObject, bindingFunction )
 {
+	this.bindingFunc = bindingFunction;
 	this.base = baseObject;
 	this.funcArray = new Array();
 }
@@ -18,10 +19,75 @@ Component.prototype.callFunction = function( funcKey, funcParams )
 
 /*******************************************************/
 
-function componentKineticAssembler( componentObject, kineticShape )
+function componentKineticPrepare( componentObject, kineticShape )
 {
 	componentObject.on("dragmove", function() {
 				shapeComponent.callFunction( "dragmove", [] );
             });
     // TODO: Define how the components function will work and finish this function
+}
+
+/***********************************************************/
+
+/* GenericKineticComponent component */
+function mouseOver() 
+{
+	document.body.style.cursor = "pointer";
+}
+
+function mouseOut() 
+{
+	document.body.style.cursor = "default";
+}
+
+function dragMove() 
+{
+     document.body.style.cursor = "pointer";
+};
+
+function dblClick( kineticShape )
+{
+	var layer = kineticShape.getLayer();
+	kineticShape.remove();
+	alert( this.i++ );
+    layer.draw();
+}
+
+function bindGenericComponent( kineticShape, componentObj )
+{
+	kineticShape.on( "dragmove",
+		function( evt )
+		{
+			componentObj.callFunction( "dragmove", [] );
+		}
+	);
+	
+	kineticShape.on( "mouseover",
+		function( evt )
+		{
+			componentObj.callFunction( "mouseOver", [] );
+		}
+	);
+	
+	kineticShape.on( "dragmove",
+		function( evt )
+		{
+			componentObj.callFunction( "dragMove", [] );
+		}
+	);
+	
+	kineticShape.on( "dblclick",
+		function( evt )
+		{
+			componentObj.callFunction( "dblClick", [kineticShape] );
+		}
+	);
+}
+
+function GenericKineticComponent( baseObj )
+{
+	var baseObj = { i :137, count : "a" };
+	Component.call( this , baseObj, bindGenericComponent );
+	this.addFunction( "dragmove", dragMove );
+	this.addFunction( "dblclick", dblclick );
 }
--- src/graphicController.js
+++ src/graphicController.js
@@ -4,6 +4,10 @@ var graphicControllerGlobals = {};
 graphicControllerGlobals.MAX_DEPTH = 15;
 graphicControllerGlobals.CANVAS_WIDTH = 800;
 graphicControllerGlobals.CANVAS_HEIGHT = 600;
+graphicControllerGlobals.stylesEnum = { DEFAULT : 0, ANDROID : 1, WINDOWS8: 2, LINUX: 3 };
+graphicControllerGlobals.currentStyle = graphicControllerGlobals.stylesEnum.DEFAULT;
+graphicControllerGlobals.styleChangers = {};
+
 
 /**
  * The GraphicController object, it will control the graphic part that uses the kinetic stage to draw and manipulate shapes.
@@ -25,6 +29,8 @@ function GraphicController( containerDOMID )
 		this.layers[i] = new Kinetic.Layer();
 		this.stage.add( this.layers[i] );
 	}
+	
+	graphicControllerGlobals.styleChangers[ graphicControllerGlobals.stylesEnum.DEFAULT ] = new KineticStyleChanger();
 }
 
 GraphicController.prototype.constructor = GraphicController;
@@ -32,10 +38,75 @@ GraphicController.prototype.constructor = GraphicController;
 
 GraphicController.prototype.createGraphicObject = function ( interfaceResource )
 {
-	var kinectGroup = new Kinetic.Group(  { x:0, y:0, draggable:true, id:interfaceResource.getId });
-	
+	var kineticShape = defaultKineticFactory( interfaceResource );
+}
+
+
+function defaultKineticFactory( interfaceResource )
+{
 	switch( interfaceResource.getResourceType )
 	{
-		
+		case( resourceTypeEnum.IR_BUTTON ):
+		{
+			var kinectGroup = new Kinetic.Group(  { x:interfaceResource.getX(), y:interfaceResource.getY(), draggable:true, id:"_button_"+id_count, name:interfaceResource.getName() });
+
+			var square1 = new Kinetic.Rect({
+			  fill: "yellow",
+			  stroke: "black",
+			  strokeWidth: 4,
+			  name: "_EXTERN_SQUARE",
+			  //draggable: true,
+			  width: interfaceResource.getWidth(),
+			  height: interfaceResource.getHeight()
+			});
+			var square2 = new Kinetic.Rect({
+			  fill: "blue",
+			  stroke: "black",
+			  strokeWidth: 4,
+			  //draggable: true,
+			  name: "_INTERN_SQUARE",
+			  x:5,
+			  y:5,
+			  width: 0.9*interfaceResource.getWidth(),
+			  height: 0.9*interfaceResource.getHeight()
+			});
+			
+			var simpleText = new Kinetic.Text({
+			  x: 25,
+			  y: 15,
+			  name: "_TEXT",
+			  text: interfaceResource.getName(),
+			  fontSize: 12,
+			  fontFamily: 'Calibri',
+			  textFill: 'black'
+			});
+			
+			kinectGroup.add( square1 );
+			kinectGroup.add( square2 );
+			kinectGroup.add( simpleText );
+			return kinectGroup;
+			break;
+		}
 	}
 }
+
+
+
+function KineticStyleChanger()
+{
+}
+
+KineticStyleChanger.prototype.constructor = KineticStyleChanger;
+
+
+KineticStyleChanger.prototype.modifyButton = function ( kineticShape )
+{
+	return kineticShape;
+}
+
+
+KineticStyleChanger.prototype.modifyWindow = function ( kineticShape )
+{
+	return kineticShape;
+}
+
